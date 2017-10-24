3b791957ed48baa74274e21a6a438d771d28653c
Jan Suchal <jan.suchal@gmail.com>
2017-10-24 18:48:36 +0200

    Add support for syncing lots of pages

--- app/jobs/sync_category_topics_job.rb
+++ app/jobs/sync_category_topics_job.rb
@@ -1,10 +1,12 @@
 class SyncCategoryTopicsJob < ApplicationJob
   queue_as :default
 
-  def perform(category_slug, sync_topic_job = SyncTopicJob)
+  def perform(category_slug, page = 0, sync_topic_job: SyncTopicJob)
     client = DiscourseApi::Client.new(ENV.fetch('REDFLAGS_DISCOURSE_URL'))
 
-    topics = client.category_latest_topics(category_slug: category_slug)
+    topics = client.category_latest_topics(category_slug: category_slug, page: page)
+
+    SyncCategoryTopicsJob.perform_later(category_slug, page + 1) if topics.count >= 30
 
     topics.each do |topic|
       sync_topic_job.perform_later(topic.fetch('id'))
--- spec/jobs/sync_category_topics_job_spec.rb
+++ spec/jobs/sync_category_topics_job_spec.rb
@@ -4,12 +4,16 @@ RSpec.describe SyncCategoryTopicsJob, type: :job do
   let(:sync_topic_job) { spy }
 
   it 'schedules topic sync jobs for all topics in category', vcr: true do
-    subject.perform('red-flags', sync_topic_job)
+    subject.perform('red-flags', sync_topic_job: sync_topic_job)
 
     expect(sync_topic_job).to have_received(:perform_later).with(4034)
     expect(sync_topic_job).to have_received(:perform_later).with(4334)
     expect(sync_topic_job).to have_received(:perform_later).with(4035)
   end
 
-  pending 'schedules next topic page when listing too long'
+  it 'schedules next topic page when listing too long', vcr: true do
+    expect(SyncCategoryTopicsJob).to receive(:perform_later).with('statne-projekty', 1)
+
+    subject.perform('statne-projekty')
+  end
 end
