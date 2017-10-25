db26e1c0b55bd6ea7aaf7430638b3ea661e31903
Pavol Zbell <pavol.zbell@gmail.com>
2017-10-25 14:26:15 +0200

    Mount Que

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
+require 'que/web'
+
+Que::Web.use(Rack::Auth::Basic) do |username, password|
+  username == ENV.fetch('ADMIN_USER') && password == ENV.fetch('ADMIN_PASSWORD')
+end
+
 Rails.application.routes.draw do
   namespace :admin do
     root to: 'pages#index'
@@ -5,6 +11,8 @@ Rails.application.routes.draw do
     resources :pages do
       put :sync, on: :collection
     end
+    
+    mount Que::Web, at: 'que'
   end
 
   root to: 'static#kitchen_sink'
