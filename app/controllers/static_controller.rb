f8100af8115d84f4bfd26991422e820670cf811b
Pavol Zbell <pavol.zbell@gmail.com>
2017-10-30 22:44:20 +0100

    Better ENV naming, drier StaticController

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -4,18 +4,25 @@ class StaticController < ApplicationController
   end
 
   def about
-    @page = Page.find(ENV.fetch('REDFLAGS_STATIC_ABOUT'))
+    render_page :about
   end
 
   def committee
-
+    render_page :committee
   end
 
   def contribute
-
+    render_page :contribute
   end
 
   def faq
+    render_page :faq
+  end
 
+  private
+
+  def render_page(name)
+    @page = Page.find(ENV.fetch("REDFLAGS_#{name.upcase}_PAGE_ID"))
+    render 'page'
   end
 end
