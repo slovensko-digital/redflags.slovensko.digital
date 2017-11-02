2060488ee48f09da217037ec44161bad05cc9199
Ernest Walzel <ewalzel@gmail.com>
2017-11-02 23:33:53 +0100

    Set site-level OpenGraph meta tags

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :set_page
+
+  def set_page
+    @page = OpenStruct.new(
+        url: request.protocol + request.host_with_port + request.path,
+        og: OpenStruct.new(
+            title: 'Red Flags od Slovensko.Digital',
+            description: 'Monitorované IT projekty a ich kolaboratívne hodnotenie metodikou Red Flags.',
+            image: view_context.image_url('temp_fb_share.png'),
+        )
+    )
+  end
 end
