dbfdd2c3f21479b1a899648cfef787ffcd5a7f02
Ernest Walzel <ewalzel@gmail.com>
2017-11-02 16:36:45 +0100

    Newsletter sign-up form. Closes #11

---
+++ app/assets/javascripts/newsletter_sign_up.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+    $('#newsletter-form').submit(function (evt) {
+        var form = $(evt.target);
+        var data = form.serialize();
+        var button = form.find('button')[0];
+        button.innerHTML = 'Prihlasujem...';
+
+        $.ajax({
+                type: 'POST',
+                url: form.data('action'),
+                data: data,
+                dataType: 'json',
+                success: function (data) {
+                    if (data.result != undefined && data.result.result == 'success') {
+                        form.remove();
+                        $('#newsletter-success').removeClass('hidden');
+                        // ga('send', 'event', 'Newsletter', 'subscribe');
+                    }
+                },
+                complete: function () {
+                    button.innerHTML = 'Prihlásiť'
+                }
+            }
+        );
+        evt.preventDefault();
+    });
+});
