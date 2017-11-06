4f7f8ed70d3965c930f18a240a2904759411aaf1
Ernest Walzel <ewalzel@gmail.com>
2017-11-06 09:45:25 +0100

    Rename @page -> @metadata for OpenGraph variable

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  before_action :set_page
+  before_action :set_metadata
 
-  def set_page
-    @page = OpenStruct.new(
-        url: request.protocol + request.host_with_port + request.path,
+  private
+
+  def set_metadata
+    @metadata = OpenStruct.new(
         og: OpenStruct.new(
+            url: request.protocol + request.host_with_port + request.path,
             title: 'Red Flags od Slovensko.Digital',
             description: 'Monitorované IT projekty a ich kolaboratívne hodnotenie metodikou Red Flags.',
             image: view_context.image_url('temp_fb_share.png'),
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,8 +3,8 @@ class ProjectsController < ApplicationController
     @project = Project.find(params[:id]).published_revision
     @rating_types_by_phase = RatingType.all.group_by(&:rating_phase)
     @ratings_by_type = @project.ratings.index_by(&:rating_type)
-    @page.og.title = "#{@project.total_score_percentage.to_i}% – #{@project.title}"
-    @page.og.description = 'Kolaboratívne hodnotenie projektu metodikou Red Flags.'
+    @metadata.og.title = "#{@project.total_score_percentage.to_i}% – #{@project.title}"
+    @metadata.og.description = 'Kolaboratívne hodnotenie projektu metodikou Red Flags.'
   end
 
   def index
