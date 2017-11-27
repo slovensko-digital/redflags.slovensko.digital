a94174cc56a208194aebf9c8cc0852b027ffc80c
Pavol Zbell <pavol.zbell@gmail.com>
2017-11-27 07:10:21 +0100

    Fix naming

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,4 +1,4 @@
-module ProjectHelper
+module ProjectsHelper
   def help_icon_if_blank(property)
     if property.blank?
       content_tag :i, nil, class: 'fa fa-question-circle text-muted', title: 'Tento údaj ešte nebol vyplnený.'
