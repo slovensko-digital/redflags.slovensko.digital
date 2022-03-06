4b8457ab58ceacb469d063322cab7b1bca4b4f05
Marek Ceľuch <celuchmarek@gmail.com>
2022-03-06 18:19:03 +0100

    Fix migration and migrate shcema version

--- db/migrate/20220306165121_drop_rating_phases.rb
+++ db/migrate/20220306165121_drop_rating_phases.rb
@@ -1,6 +1,6 @@
 class DropRatingPhases < ActiveRecord::Migration[5.1]
   def change
-    remove_index :rating_types, :index_rating_types_on_rating_phase_id
+    remove_index :rating_types, :rating_phase_id
     remove_foreign_key :rating_types, :rating_phases
     remove_column :rating_types, :rating_phase_id
     
