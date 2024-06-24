4e57f28b6c5b7e1ed2d0ba2fa7cf48f4bb26f494
Tomáš Drga <tomasxdrga@gmail.com>
2024-06-24 13:34:27 +0200

    Bug in the published migration

--- db/migrate/20240611153648_add_published_to_project_revisions.rb
+++ db/migrate/20240611153648_add_published_to_project_revisions.rb
@@ -3,8 +3,10 @@ class AddPublishedToProjectRevisions < ActiveRecord::Migration[5.1]
     add_column :phase_revisions, :published, :boolean, default: false
 
     Project.find_each do |project|
-      project.phases.revisions.find_each do |revision|
-        revision.update_attribute(:published, project.published_revision_id == revision.id)
+      project.phases.find_each do |phase|
+        phase.revisions.find_each do |revision|
+          revision.update_attribute(:published, project.published_revision_id == revision.id)
+        end
       end
     end
   end
