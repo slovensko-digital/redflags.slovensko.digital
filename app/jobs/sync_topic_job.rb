0a71a2830e1c25772a55c7bc373156645249655b
Tomáš Drga <tomasxdrga@gmail.com>
2024-06-24 14:23:32 +0200

    Fix

--- app/jobs/sync_topic_job.rb
+++ app/jobs/sync_topic_job.rb
@@ -13,6 +13,7 @@ class SyncTopicJob < ApplicationJob
         phase_type = PhaseType.find_by(name: 'Prípravná fáza')
         new_phase = Phase.find_or_create_by(project: project, phase_type: phase_type)
         new_page.phase = new_phase
+        new_page.save!
       end
 
       version = topic['post_stream']['posts'].first['version']
