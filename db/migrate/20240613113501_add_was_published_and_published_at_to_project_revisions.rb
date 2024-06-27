d63b432a94a2d3a335c301f886f95e771a790cf6
Tomáš Drga <tomasxdrga@gmail.com>
2024-06-27 10:32:16 +0200

    Migration separete updates

--- db/migrate/20240613113501_add_was_published_and_published_at_to_project_revisions.rb
+++ db/migrate/20240613113501_add_was_published_and_published_at_to_project_revisions.rb
@@ -3,10 +3,12 @@ class AddWasPublishedAndPublishedAtToProjectRevisions < ActiveRecord::Migration[
     add_column :phase_revisions, :was_published, :boolean, default: false
     add_column :phase_revisions, :published_at, :datetime
 
+    Phase.reset_column_information
     Phase.find_each do |phase|
       if phase.published_revision.present?
         published_revision = phase.published_revision
-        published_revision.update_attributes!(was_published: true, published_at: published_revision.revision.created_at)
+        published_revision.update!(was_published: true)
+        published_revision.update!(published_at: published_revision.revision.created_at)
       end
     end
   end
